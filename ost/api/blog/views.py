b8307aa4e9f7b1c28fa06b8fdc1720e9c65337d6
AcidInvader <124760286+AcidInvader@users.noreply.github.com>
2023-11-17 01:37:43 +0300

    made delete view

--- ost/api/blog/views.py
+++ ost/api/blog/views.py
@@ -2,6 +2,7 @@ from rest_framework.generics import CreateAPIView, ListAPIView
 from blog.models import Article, Comment
 from django.db.models import QuerySet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import DestroyAPIView
 
 from . import serializers
 
@@ -11,8 +12,14 @@ class ArticleCreateView(CreateAPIView):
     serializer_class = serializers.ArticleCreateSerializer
 
 
+class ArticleDeleteView(DestroyAPIView):
+    permission_classes = ()
+    serializer_class = serializers.ArticleCreateSerializer
+    queryset = Article.objects.all()
+
+
 class ArticleDetailView(ListAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = ()
     serializer_class = serializers.ArticleDetailSerializer
 
     def get_queryset(self) -> QuerySet[Article]:
@@ -25,7 +32,7 @@ class CommentCreateView(CreateAPIView):
 
 
 class CommentShowView(ListAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = ()
     serializer_class = serializers.CommentShowSerializer
 
     def get_queryset(self):
--- ost/ost/settings.py
+++ ost/ost/settings.py
@@ -85,10 +85,6 @@ WSGI_APPLICATION = 'ost.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # },
     "default": {
             "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
             "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
